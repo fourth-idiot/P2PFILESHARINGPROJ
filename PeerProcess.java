29ccf087554301109df0100983798baa364d2e56
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-12-04 18:39:51 -0500

    Add code to start remote peers

--- PeerProcess.java
+++ PeerProcess.java
@@ -2,6 +2,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ExecutorService;
+import java.nio.file.Paths;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,12 +10,20 @@ import org.apache.logging.log4j.Logger;
 public class PeerProcess {
     private static final Logger LOGGER = LogManager.getLogger(PeerProcess.class);
 
-    public static void main(String[] args) {
+    private static void createRequiredDirStructure(PeerInfoCfg.PeerInfo peerInfo, String workingDir, String inputFileName) throws Exception {
+        String[] createDirectoryStructureCommand = new String[] {"sh", "-c", String.format("mkdir -p %s", Paths.get(workingDir, String.format("peer_%d", peerInfo.getId())).toString())};
+        Process createDirectoryStructureProcess = Runtime.getRuntime().exec(createDirectoryStructureCommand);
+        createDirectoryStructureProcess.waitFor();
         
+        if(peerInfo.getHasFile()) {
+            String[] copyInputFileCommand = new String[] {"sh", "-c", String.format("cp %s %s", Paths.get(workingDir, inputFileName).toString(), Paths.get(workingDir, String.format("peer_%d", peerInfo.getId())).toString())};
+            Process copyInputFileProcess = Runtime.getRuntime().exec(copyInputFileCommand);
+            copyInputFileProcess.waitFor();
+        }   
+    }
+
+    public static void main(String[] args) {
         LOGGER.info("Inside main method");
-        // System.setProperty("logFilePath", "path to the log folder");
-        String myString = System.getProperty("logFilePath");
-        System.out.println(myString);
 
         // Read the current peer id
         int id = Integer.parseInt(args[0]);
@@ -33,6 +42,13 @@ public class PeerProcess {
         PeerInfoCfg peerInfoCfg = new PeerInfoCfg();
         peerInfoCfg.parse(peerInfoLines);
         System.out.println(peerInfoCfg);
+
+        // Create folder structure
+        try {
+            createRequiredDirStructure(peerInfoCfg.getPeer(id), Constants.WORKING_DIR, commonCfg.getFileName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         // Create peer instance
         ExecutorService executorService = Executors.newFixedThreadPool(16);
