7f8d1d44758aa5796276898f8b9f2257998d3ca0
Nikhil Mukesh Saoji <nm.saoji@gmail.com>
2022-12-03 23:47:28 -0500

    Add empty MessageSender class

---
+++ MessageSender.java
@@ -0,0 +1,13 @@
+import java.io.OutputStream;
+
+public class MessageSender implements Runnable {
+    MessageSender(OutputStream outputStream, byte[] msg) {
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
